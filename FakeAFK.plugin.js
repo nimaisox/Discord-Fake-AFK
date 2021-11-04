51f452706092831a96e3e0e88185cdf2b9912e5c
nimaisox <61984673+nimaisox@users.noreply.github.com>
2021-11-04 22:40:22 +0330

    Add files via upload

---
+++ FakeAFK.plugin.js
@@ -0,0 +1,30 @@
+/**
+ * @name FakeAFK
+ * @version 1.0.0
+ * @description Fake AFK Discord Buger
+ * @author nimaisox
+ */
+ module.exports = class FakeAFK{
+   
+ load() { }
+ start() {
+  
+var text = new TextDecoder("utf-8");
+  
+WebSocket.prototype.original = WebSocket.prototype.send;
+WebSocket.prototype.send = function(data) {
+    if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
+        if (text.decode(data).includes("self_deaf")) {
+            console.log("Run");
+            data = data.replace('"self_mute":false', 'Discord');
+            console.log("Fake AFK On");
+        }
+    }
+    WebSocket.prototype.original.apply(this, [data]);
+}
+window.BdApi.alert("success",`Stop the plugin!`);
+}
+stop(){
+
+}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Discord Fake AFK
+Plugin for cheating
+
+# How to Install
+Get the Fake AFK file and add it to the Better Discord plugin file
+
+# How To Use
+To use this plugin, you must first afk (Mute and Deafen). Activate and deactivate the plugin and enjoy. If you get a bug you need to restart the incompatibility to fix it
