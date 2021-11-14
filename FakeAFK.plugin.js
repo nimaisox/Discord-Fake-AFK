22c3ef3b4f8272ce0a578a5064f1c66a8b768d2c
nimaisox <61984673+nimaisox@users.noreply.github.com>
2021-11-14 17:53:42 +0330

    Update FakeAFK.plugin.js

--- FakeAFK.plugin.js
+++ FakeAFK.plugin.js
@@ -6,7 +6,6 @@
  * @authorId 631463369458843668
  * @invite DStAkkypnm
  * @source https://github.com/nimaisox/Discord-Fake-AFK
- * @updateUrl https://github.com/nimaisox/Discord-Fake-AFK/blob/main/FakeAFK.plugin.js
  */
  module.exports = class FakeAFK{
    
